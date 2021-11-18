e501cd8daf19f5cc32dd8cbab3cf3d1a3b9b2fb7
kevinmirama <66650690+kevinmirama@users.noreply.github.com>
2021-11-17 22:38:30 -0500

    Update urls.py

--- libreriaproject/urls.py
+++ libreriaproject/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('login/',        TokenObtainPairView.as_view()),
     path('refresh/',      TokenRefreshView.as_view()),
     path('user/',         views.UserCreateView.as_view()),
+    path('verifytokenView/',    views.verifyTokenView.as_view()),
 ]
